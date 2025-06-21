7a1495c4dc4d1b5dbfd023863819e97853a4dbe0
Brendan Jackman <bhenryj0117@gmail.com>
2025-06-21 14:35:56 +0200

    parser/jsonpath: Be sloppier about expressions
    JSONPath has a "filtering" feature where you can do stuff like:
    
    $.jobs[?(@.jobname == "randread_tmpfs")].read.iops
    
    And it will extract the read.iops field from all jobs where the
    jobname is "randread_tmpfs". However it seems if you use this feature
    it insists on returning a list.
    
    So, just allow the expressions to return lists of length 1.
    
    Since I can't be bothered to figure out this gval thing, this
    requires doing less validation, we'll just recompile the JSONPath for
    every Extract() call.

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -2,14 +2,12 @@
 package parser
 
 import (
-	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"regexp"
 	"strings"
 
-	"github.com/PaesslerAG/gval"
 	"github.com/PaesslerAG/jsonpath"
 	"github.com/bjackman/falba/internal/falba"
 )
@@ -161,16 +159,10 @@ func (p *RegexpExtractor) String() string {
 type JSONPathExtractor struct {
 	resultType falba.ValueType
 	expression string
-	selector   *gval.Evaluable
 }
 
 func NewJSONPathExtractor(expr string, resultType falba.ValueType) (*JSONPathExtractor, error) {
-	selector, err := jsonpath.New(expr)
-	if err != nil {
-		return nil, fmt.Errorf("parsing JSONPath expression: %v", err)
-	}
 	return &JSONPathExtractor{
-		selector:   &selector,
 		expression: expr,
 		resultType: resultType,
 	}, nil
@@ -185,23 +177,56 @@ func (e *JSONPathExtractor) Extract(artifact *falba.Artifact) (falba.Value, erro
 	if err := json.Unmarshal(content, &obj); err != nil {
 		return nil, fmt.Errorf("%w: unmarshalling from JSON: %v", ErrParseFailure, err)
 	}
+
+	// We'd prefer to pre-compile the JSONPath expression but then evaluating it
+	// gies you a gval.Evaluable which I can't be bothered to deal with, I don't
+	// know how to get non-scalar objects out of it. So instead we just evaluate
+	// it as string "at runtime" which gives us an untyped result we can
+	// manually try to squash into the type we want.
+	got, err := jsonpath.Get(e.expression, obj)
+	if err != nil {
+		// I believe this error must mean there's something wrong with the
+		// expression, not just that it didn't match anything. So this is fatal.
+		return nil, fmt.Errorf("failed to evaluate JSONPath: %v", err)
+	}
+
+	var gotVal any
+	switch got := got.(type) {
+	case []any:
+		// JSONPath seems to be weird and annoying when you use its
+		// filtering functionality, AFAICS it doesn't have a built-in
+		// facility to extract an individual value. So we just allow it to
+		// return a slice of length 1.
+		if len(got) != 1 {
+			return nil, fmt.Errorf("%w: JSONPath returned %d values, expected 1", ErrParseFailure, len(got))
+		}
+		gotVal = got[0]
+	default:
+		gotVal = got
+	}
+
 	switch e.resultType {
 	case falba.ValueInt:
-		val, err := e.selector.EvalInt(context.Background(), obj)
-		if err != nil {
-			return nil, fmt.Errorf("%w: evaluating JSONPath as int: %v", ErrParseFailure, err)
+		// JSON doesn't have proper numeric types so we can't actually enforce
+		// that the value is an integer. Just squash it into one.
+		switch v := gotVal.(type) {
+		case float64:
+			return &falba.IntValue{Value: int64(v)}, nil
+		case int:
+			return &falba.IntValue{Value: int64(v)}, nil
+		default:
+			return nil, fmt.Errorf("%w: JSONPath returned %T, wanted numeric", ErrParseFailure, gotVal)
 		}
-		return &falba.IntValue{Value: int64(val)}, nil
 	case falba.ValueString:
-		val, err := e.selector.EvalString(context.Background(), obj)
-		if err != nil {
-			return nil, fmt.Errorf("%w: evaluating JSONPath as int: %v", ErrParseFailure, err)
+		val, ok := gotVal.(string)
+		if !ok {
+			return nil, fmt.Errorf("%w: JSONPath returned %T, wanted string", ErrParseFailure, gotVal)
 		}
 		return &falba.StringValue{Value: val}, nil
 	case falba.ValueFloat:
-		val, err := e.selector.EvalFloat64(context.Background(), obj)
-		if err != nil {
-			return nil, fmt.Errorf("%w: evaluating JSONPath as int: %v", ErrParseFailure, err)
+		val, ok := gotVal.(float64)
+		if !ok {
+			return nil, fmt.Errorf("%w: JSONPath returned %T, wanted float64", ErrParseFailure, gotVal)
 		}
 		return &falba.FloatValue{Value: val}, nil
 	default:
